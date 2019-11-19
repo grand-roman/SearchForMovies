aa4dc9429a25d25e089b5561c1f52c4aa5d2eee2
Roman <31464179+grand-roman@users.noreply.github.com>
2019-11-19 20:22:45 +0700

    let

--- src/main.js
+++ src/main.js
@@ -481,104 +481,104 @@ const getSearchTemplate = () => {
     </button>`);
 };
 
-const getStatisticTemplate = () => {
-  return (`
-  <p class="statistic__rank">
-    Your rank
-    <img class="statistic__img"
-      src="images/bitmap@2x.png"
-      alt="Avatar"
-      width="35" height="35">
-    <span class="statistic__rank-label">
-      Sci-Fighter
-    </span>
-  </p>
-  <form action="https://echo.htmlacademy.ru/"
-    method="get"
-    class="statistic__filters">
-    <p class="statistic__filters-description">
-      Show stats:
-    </p>
-    <input type="radio"
-      class="statistic__filters-input visually-hidden"
-      name="statistic-filter"
-      id="statistic-all-time"
-      value="all-time"
-      checked
-    >
-    <label for="statistic-all-time"
-      class="statistic__filters-label">
-      All time
-    </label>
-    <input type="radio"
-      class="statistic__filters-input visually-hidden"
-      name="statistic-filter"
-      id="statistic-today"
-      value="today"
-    >
-    <label for="statistic-today"
-      class="statistic__filters-label">
-      Today
-    </label>
-    <input type="radio"
-      class="statistic__filters-input visually-hidden"
-      name="statistic-filter"
-      id="statistic-week"
-      value="week"
-    >
-    <label for="statistic-week"
-      class="statistic__filters-label">
-      Week
-    </label>
-    <input type="radio"
-      class="statistic__filters-input visually-hidden"
-      name="statistic-filter"
-      id="statistic-month"
-      value="month"
-    >
-    <label for="statistic-month"
-      class="statistic__filters-label">
-      Month
-    </label>
-    <input type="radio"
-      class="statistic__filters-input visually-hidden"
-      name="statistic-filter"
-      id="statistic-year"
-      value="year"
-    >
-    <label for="statistic-year"
-      class="statistic__filters-label">
-      Year
-    </label>    
-  </form>
-  <ul class="statistic__text-list">
-    <li class="statistic__text-item">
-      <h4 class="statistic__item-title">You watched</h4>
-      <p class="statistic__item-text">
-        22 
-        <span class="statistic__item-description">movies</span>
-      </p>
-    </li>
-    <li class="statistic__text-item">
-      <h4 class="statistic__item-title">Total duration</h4>
-      <p class="statistic__item-text">
-        130
-        <span class="statistic__item-description">h</span>
-        22
-        <span class="statistic__item-description">m</span>
-      </p>
-    </li>
-    
-    <li class="statistic__text-item">
-      <h4 class="statistic__item-title">Top genre</h4>
-      <p class="statistic__item-text">Sci-Fi</p>
-    </li>
-  </ul>
-  <div class="statistic__chart-wrap">
-    <canvas class="statistic__chart" width="1000"></canvas>
-  </div>
-  `);
-};
+// const getStatisticTemplate = () => {
+//   return (`
+//   <p class="statistic__rank">
+//     Your rank
+//     <img class="statistic__img"
+//       src="images/bitmap@2x.png"
+//       alt="Avatar"
+//       width="35" height="35">
+//     <span class="statistic__rank-label">
+//       Sci-Fighter
+//     </span>
+//   </p>
+//   <form action="https://echo.htmlacademy.ru/"
+//     method="get"
+//     class="statistic__filters">
+//     <p class="statistic__filters-description">
+//       Show stats:
+//     </p>
+//     <input type="radio"
+//       class="statistic__filters-input visually-hidden"
+//       name="statistic-filter"
+//       id="statistic-all-time"
+//       value="all-time"
+//       checked
+//     >
+//     <label for="statistic-all-time"
+//       class="statistic__filters-label">
+//       All time
+//     </label>
+//     <input type="radio"
+//       class="statistic__filters-input visually-hidden"
+//       name="statistic-filter"
+//       id="statistic-today"
+//       value="today"
+//     >
+//     <label for="statistic-today"
+//       class="statistic__filters-label">
+//       Today
+//     </label>
+//     <input type="radio"
+//       class="statistic__filters-input visually-hidden"
+//       name="statistic-filter"
+//       id="statistic-week"
+//       value="week"
+//     >
+//     <label for="statistic-week"
+//       class="statistic__filters-label">
+//       Week
+//     </label>
+//     <input type="radio"
+//       class="statistic__filters-input visually-hidden"
+//       name="statistic-filter"
+//       id="statistic-month"
+//       value="month"
+//     >
+//     <label for="statistic-month"
+//       class="statistic__filters-label">
+//       Month
+//     </label>
+//     <input type="radio"
+//       class="statistic__filters-input visually-hidden"
+//       name="statistic-filter"
+//       id="statistic-year"
+//       value="year"
+//     >
+//     <label for="statistic-year"
+//       class="statistic__filters-label">
+//       Year
+//     </label>
+//   </form>
+//   <ul class="statistic__text-list">
+//     <li class="statistic__text-item">
+//       <h4 class="statistic__item-title">You watched</h4>
+//       <p class="statistic__item-text">
+//         22
+//         <span class="statistic__item-description">movies</span>
+//       </p>
+//     </li>
+//     <li class="statistic__text-item">
+//       <h4 class="statistic__item-title">Total duration</h4>
+//       <p class="statistic__item-text">
+//         130
+//         <span class="statistic__item-description">h</span>
+//         22
+//         <span class="statistic__item-description">m</span>
+//       </p>
+//     </li>
+//
+//     <li class="statistic__text-item">
+//       <h4 class="statistic__item-title">Top genre</h4>
+//       <p class="statistic__item-text">Sci-Fi</p>
+//     </li>
+//   </ul>
+//   <div class="statistic__chart-wrap">
+//     <canvas class="statistic__chart" width="1000"></canvas>
+//   </div>
+//   `);
+// };
 
 const getSortTemplate = (obj) => {
   return (Object.keys(obj).map((key) => (
@@ -595,15 +595,13 @@ const search = header.querySelector(`.search`);
 const profile = header.querySelector(`.profile`);
 const main = document.querySelector(`.main`);
 const mainNavigation = main.querySelector(`.main-navigation`);
-const statistic = main.querySelector(`.statistic`);
+// const statistic = main.querySelector(`.statistic`);
 const sort = main.querySelector(`.sort`);
 const films = main.querySelector(`.films`);
 
 const filmsDetails = document.querySelector(`.film-details`);
 
-console.log(document.querySelector(`.film-details`));
-
-render(filmsDetails, getFilmDetailsTemplate);
+render(filmsDetails, getFilmDetailsTemplate());
 
 const filmDetailsCloseBtn = filmsDetails.querySelector(`.film-details__close-btn`);
 
@@ -611,7 +609,7 @@ render(search, getSearchTemplate());
 render(profile, getProfileTemplate());
 
 render(mainNavigation, getMainNavigationTemplate());
-render(statistic, getStatisticTemplate());
+// render(statistic, getStatisticTemplate());
 render(sort, getSortTemplate(sortType));
 
 filmTitles.forEach((obj)=> {
