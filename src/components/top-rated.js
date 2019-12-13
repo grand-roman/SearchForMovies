05e870f64c51f3ec589954b5dc18b66d16d8dc23
nsis <grand-roman@yandex.ru>
2019-12-13 22:32:53 +0700

    добавлен новый компонент top-rated

---
+++ src/components/top-rated.js
@@ -0,0 +1,15 @@
+import AbstractComponent from "./abstract-component";
+
+class TopRated extends AbstractComponent {
+  constructor() {
+    super();
+  }
+  getTemplate() {
+    return `<section class="films-list--extra">
+          <h2 class="films-list__title">Top rated</h2>
+          <div class="films-list__container">
+          </div>
+        </section>`;
+  }
+}
+export default TopRated;
