e17bd2445c338d8e6d1554bd48bcdc9632de42af
nsis <grand-roman@yandex.ru>
2019-12-13 22:30:26 +0700

    изменен компонент film-list

--- src/components/film-list.js
+++ src/components/film-list.js
@@ -1,31 +1,18 @@
-import {getFilmsListTemplate} from './film-list-template.js';
-import AbstractComponent from './abstract-component.js';
+import AbstractComponent from "./abstract-component";
 
 class FilmList extends AbstractComponent {
-
-  constructor({isExtra, isVisuallyHidden, title, films,
-    isButton, id}) {
-
+  constructor() {
     super();
-    this._isExtra = isExtra;
-    this._isVisuallyHidden = isVisuallyHidden;
-    this._title = title;
-    this._films = films;
-    this._isButton = isButton;
-    this._id = id;
-
   }
-
-  get template() {
-    return getFilmsListTemplate(
-        this._isExtra,
-        this._isVisuallyHidden,
-        this._title,
-        this._isButton,
-        this._id
-    );
+  getTemplate() {
+    return `<section class="films">
+        <section class="films-list">
+          <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+          <div class="films-list__container">
+          </div>
+        </section>
+      </section>`;
   }
-
 }
 
 export default FilmList;
