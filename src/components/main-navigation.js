f74cbacd68cca8172ba3fecae4ee69360928be11
nsis <grand-roman@yandex.ru>
2019-12-13 22:28:44 +0700

    изменен компонент main-navigation

--- src/components/main-navigation.js
+++ src/components/main-navigation.js
@@ -1,17 +1,23 @@
-import {getMainNavigationTemplate} from './main-navigation-template.js';
-import AbstractComponent from './abstract-component.js';
+import AbstractComponent from "./abstract-component";
 
 class MainNavigation extends AbstractComponent {
-
-  constructor(menuTypes) {
+  constructor(historyCount, watchlistCount, favorites) {
     super();
-    this._menuTypes = menuTypes;
+    this._historyCount = historyCount;
+    this.watchlistCount = watchlistCount;
+    this.favorites = favorites;
   }
-
-  get template() {
-    return getMainNavigationTemplate(this._menuTypes);
+  getTemplate() {
+    return `<nav class="main-navigation">
+        <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
+        <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${this.watchlistCount
+}</span></a>
+        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${this._historyCount
+}</span></a>
+        <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${this.favorites
+}</span></a>
+        <a href="#stats" class="main-navigation__item main-navigation__item--additional">Stats</a>
+      </nav>`;
   }
-
 }
-
 export default MainNavigation;
