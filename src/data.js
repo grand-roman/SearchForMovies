cf4e0d1a4308240c86375117f11b71f9dae085a6
Roman <31464179+grand-roman@users.noreply.github.com>
2019-12-18 11:49:11 +0700

    Обновил Моки

--- src/data.js
+++ src/data.js
@@ -1,5 +1,4 @@
 import {generatorRandom} from './utils.js';
-
 const filmData = {
   filmTitle: [`Die hard`, `The Godfather`, `Pulp Fiction`, `Star Wars`, `Schindler's List`, `Fight Club`, `Saving Private Ryan`, `Back to the Future`,
     `The Lord of the Rings - The Return of the King`, `Titanic`, `Terminator 2`, `The Departed`, `Avatar`, `Home Alone`, `The Fifth Element`],
@@ -49,36 +48,43 @@ Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis 
   hour: Math.round(generatorRandom.generateRandomNumber(0, 2)),
   minutes: Math.round(generatorRandom.generateRandomNumber(0, 60)),
 };
-export const generateFilmData = () => {
+const generateDesk = () => {
+  const deskStr = generatorRandom.splitStr(filmData.desciption);
+  deskStr.length = Math.round(generatorRandom.generateRandomNumber(1, 3));
+  return deskStr.join();
+};
+const generateTime = () => {
+  let hour = Math.round(generatorRandom.generateRandomNumber(0, 2)) + `h`;
+  let minutes = Math.round(generatorRandom.generateRandomNumber(0, 60));
+  if (hour === `0h`) {
+    hour = ``;
+  }
+  return `${hour} ${minutes}m`;
+};
+const generateFilmData = () => {
   return {
     filmTitle: filmData.filmTitle[generatorRandom.generateRandomCount(filmData.filmTitle.length)],
     ratings: parseFloat(generatorRandom.generateRandomNumber(1, 9)),
     genre: filmData.genre[generatorRandom.generateRandomCount(filmData.genre.length)],
     posters: filmData.posters[generatorRandom.generateRandomCount(filmData.posters.length)],
     year: Math.floor(generatorRandom.generateRandomNumber(filmData.year[0], filmData.year[1])),
-    desciption: () => {
-      const deskStr = generatorRandom.splitStr(filmData.desciption);
-      deskStr.length = Math.round(generatorRandom.generateRandomNumber(1, 3));
-      return deskStr.join();
-    },
+    desciption: generateDesk(),
     id: Math.round(generatorRandom.generateRandomNumber(1, 1000)),
     director: filmData.director[generatorRandom.generateRandomCount(filmData.director.length)],
     writers: filmData.writers,
     actors: filmData.actors.splice(generatorRandom.generateRandomCount(8), generatorRandom.generateRandomNumber(2, 7)),
     country: filmData.country[Math.round(generatorRandom.generateRandomCount(4))],
-    comments: Math.round(generatorRandom.generateRandomNumber(0, 10)),
-    isWatchlist: Boolean(Math.round(Math.random())),
-    isHistory: Boolean(Math.round(Math.random())),
-    isFavorites: Boolean(Math.round(Math.random())),
-    runtime: () => {
-      let hour = Math.round(generatorRandom.generateRandomNumber(0, 2)) + `h`;
-      let minutes = Math.round(generatorRandom.generateRandomNumber(0, 60));
-      if (hour === `0h`) {
-        hour = ``;
-      }
-      return `${hour} ${minutes}m`;
-    },
+    countComments: Math.round(generatorRandom.generateRandomNumber(0, 10)),
+    watchlist: Boolean(Math.round(Math.random())),
+    watched: Boolean(Math.round(Math.random())),
+    favorites: Boolean(Math.round(Math.random())),
+    runtime: generateTime(),
+    comments: getArrayOfComments(),
   };
+};
+const commentData = {
+  autors: [`Sasha Grey`, `Jonh`, `Alexandr`, `Ivan`, `Bill`],
+  avatars: [`angry.png`, `puke.png`, `sleeping.png`, `smile.png`, `trophy.png`]
 };
 const generateFilmList = () => Math.round(generatorRandom.generateRandomNumber(5, 25));
 const generateMenuCount = () => Math.round(generatorRandom.generateRandomNumber(0, 20));
@@ -93,7 +99,42 @@ export const generateRank = () => {
     strRank = `movie buff`;
   } return strRank;
 };
+const getRandomElement = (array) => { // Рандомный элемент из массива
+  return array[getRandomNumber(0, array.length)];
+};
+
+const getRandomNumber = (from, to) => { // Функция получения рандомного числа в диапазоне
+  return Math.floor(Math.random() * (from - to)) + to;
+};
+
+const randomDate = (start, end) => { // Функция получения рандомной даты в диапазоне
+  const date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  return [date.getDate(), date.toLocaleDateString(`en-US`, {month: `long`}), date.getFullYear()];
+};
+const getComments = () => ({// Комментарий
+  avatar: getRandomElement(commentData.avatars),
+  text: getDescription(filmData.desciption),
+  autor: getRandomElement(commentData.autors),
+  date: randomDate(new Date(2018, 0, 1), new Date(2019, 0, 1))
+});
+const getDescription = (string) => { // Функция получения рандомного описания фильма
+  const newDescription = [];
+  const array = string.split(`.`);
+
+  for (let i = 0; i < getRandomNumber(1, 4); i++) {
+    newDescription.push(array[getRandomNumber(0, array.length)]);
+  }
+  return newDescription.join(`.`);
+};
+const getArrayOfComments = () => { // Массив комментов
+  return new Array(getRandomNumber(3, 6)).fill().map(getComments);
+};
+
 export const historyCount = generateMenuCount();
 export const watchlistCount = generateMenuCount();
 export const favorites = generateMenuCount();
 export const totalfilm = generateFilmList();
+export const arrFilm = [];
+for (let i = 0; i < totalfilm; i++) {
+  arrFilm.push(generateFilmData());
+}
